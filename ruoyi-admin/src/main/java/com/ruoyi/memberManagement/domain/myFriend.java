d4a16e750ded4ea47f17a009df0b44d0d0c87b99
AiHaibara <3278661092@qq.com>
2025-06-20 11:44:31 +0800

    Initial commit

---
+++ ruoyi-admin/src/main/java/com/ruoyi/memberManagement/domain/myFriend.java
@@ -0,0 +1,35 @@
+package com.ruoyi.memberManagement.domain;
+
+import java.util.Date;
+
+public class myFriend {
+    private Long friendId;
+    private String friendName;
+    private Date createTime;
+
+    public Long getFriendId() {
+        return friendId;
+    }
+
+    public void setFriendId(Long friendId) {
+        this.friendId = friendId;
+    }
+
+    public String getFriendName() {
+        return friendName;
+    }
+
+    public void setFriendName(String friendName) {
+        this.friendName = friendName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+
+}
